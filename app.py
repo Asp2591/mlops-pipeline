e346ea07c2f087dd7a7f6277b5151a101f8b446c
Asp2591 <patkaratharva75@gmail.com>
2026-02-25 09:34:52 +0530

    first commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "GCP MLOps Pipeline Running!"
+    return "GCP MLOps Pipeline Running with github action!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
